a67a2d79ccd167e357161990433fe31e9e5ce4cd
Sebastian M <kontakt@web-harmony.pl>
2017-09-02 20:52:34 +0200

    some stuf working

--- src/include/web-notification-class.php
+++ src/include/web-notification-class.php
@@ -1,10 +1,6 @@
 <?php
 defined( 'ABSPATH' ) or die( 'Not Authorized!' );
 
-include_once 'push-notification/WebPush.php';
-use Minishlink\WebPush\WebPush;
-
-
 class Web_Notification {
 
   private $settings;
@@ -68,6 +64,9 @@ class Web_Notification {
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
     dbDelta( $sql );
 
+    //setup default notification sender url
+    add_option( 'web-notification_main_options[notificationURL]', 'https://fcm.googleapis.com/fcm/send');
+
    }
   /**
    * Plugin uninstall function
@@ -129,10 +128,11 @@ class Web_Notification {
 
     add_settings_section( 'main', __('Auth API Settings', 'web-notification'), array( $this, 'main_section_callback' ), 'web-notification-settings' );
 
+    add_settings_field( 'notificationURL', 'API url', array( $this, 'api_url_callback' ), 'web-notification-settings', 'main' );
     add_settings_field( 'serverKey', 'Public key', array( $this, 'server_key_callback' ), 'web-notification-settings', 'main' );
     add_settings_field( 'privateKey', 'Private key', array( $this, 'private_key_callback' ), 'web-notification-settings', 'main' );
+    add_settings_field( 'senderID', 'Sender ID', array( $this, 'sender_id_callback' ), 'web-notification-settings', 'main' );
     add_settings_field( 'autoSend', 'Automaticly send notification after Publish', array( $this, 'auto_send_callback' ), 'web-notification-settings', 'main' );
-
   }
 
   /**
@@ -143,6 +143,16 @@ class Web_Notification {
     // return _e( 'Start adding from here you plugin settings.', 'web-notification' );
   }
 
+  /**
+   * Create the option html input
+   * @return html
+   */
+  function api_url_callback() {
+    return printf(
+      '<input type="text" id="notificationURL" name="web-notification_main_options[notificationURL]" value="%s" ',
+      isset( $this->settings['notificationURL'] ) ? esc_attr( $this->settings['notificationURL']) : ''
+    );
+  }
   /**
    * Create the option html input
    * @return html
@@ -160,8 +170,18 @@ class Web_Notification {
    */
   function private_key_callback() {
     return printf(
-      '<input type="text" id="privateKey" style="width:328px" name="web-notification_main_options[privateKey]" value="%s" />',
+      '<input type="text" id="privateKey" name="web-notification_main_options[privateKey]" value="%s" />',
       isset( $this->settings['privateKey'] ) ? esc_attr( $this->settings['privateKey']) : ''
+    );
+  }
+  /**
+   * Create the option html input
+   * @return html
+   */
+  function sender_id_callback() {
+    return printf(
+      '<input type="text" id="senderID" name="web-notification_main_options[senderID]" value="%s" />',
+      isset( $this->settings['senderID'] ) ? esc_attr( $this->settings['senderID']) : ''
     );
   }
   /**
@@ -211,16 +231,12 @@ class Web_Notification {
     ob_start(); ?>
 
     <div class="wrap">
-
-      <div class="card">
-
+      <div class="card header">
         <h1><?php _e( 'Web notification', 'web-notification' ); ?></h1>
-
         <p><?php _e( 'Start from here to build you awesome plugin, using this basic setup.', 'web-notification' ); ?></p>
-
       </div>
       <div class="plugin-row">
-        <div class="card">
+        <div class="card" style="margin-right:20px">
           <?php $this->settings = get_option( 'web-notification_main_options' ); ?>
           <form method="post" action="options.php">
             <?php settings_fields( 'web-notification-settings-group' ); ?>
@@ -230,7 +246,7 @@ class Web_Notification {
           </form>
         </div>
         <div>
-          <div class="card" style="margin-left:20px">
+          <div class="card" >
             <h2><?php _e( 'Test your notification', 'web-notification-test' ); ?></h2>
             <!-- <form method="post" action="/"> -->
               <textarea cols="50" rows="5" id="notification_test" name="web-notification_main_options[notification_test]"></textarea>
@@ -251,39 +267,37 @@ class Web_Notification {
     // {"body": "tekst", "link":"http://www.ujk.edu.pl"}
 
     if($_POST['web-notification_main_options']['autoSend']){
-      //send notification
 
+      $settings = get_option( 'web-notification_main_options' );
+
+      //send notification
       $title = get_the_title( $post_id );
       $link = get_permalink($post_id);
       
       $subs = $wpdb->get_results("SELECT sub FROM ".$this->table_name);
 
-      
       foreach ($subs as $notification) {
         $notification = json_decode($notification->sub);
-        var_dump($notification);
 
-        $server_key = 'BNMM35rJlOQzgZg_bx8ggoS5l2ddc1SirREEUg5D5Qi8YNSjstZhyAfNcRLdM8Rd4G5hsZRi8Uy-M93nK0fPu7g=';
-			
         $fields = array();
         $fields['data'] = (object)["body"=>$title, "link"=>$link];
         // if(is_array($notification)){
         //   $fields['registration_ids'] = $notification;
         // }else{
-        //   $fields['to'] = $notification;
+          $fields['to'] = $notification->endpoint;
         // }
         //header with content_type api key
         $headers = array(
-        'Content-Type:application/json',
-              'Authorization:key='.$server_key
+          'Content-Type:application/json',
+          'Authorization:key='.$settings['serverKey']
         );
         //CURL request to route notification to FCM connection server (provided by Google)			
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $notification->endpoint);
+        curl_setopt($ch, CURLOPT_URL, $settings['notificationURL']);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
         $result = curl_exec($ch);
@@ -296,8 +310,6 @@ class Web_Notification {
     }
     exit();
   }
-
 }
-// {"endpoint":"https://fcm.googleapis.com/fcm/send/eo1wjTLNl64:APA91bGg5mp7oPfcxy6kTwiXt9A5-PC9hwLTodzcqNdTgvEjqi2dvvC-blyJQ0TpzeQi3-qCGrp3vPfZ2ZAdcze0eVpyZv3JQRC5gwADawQCLdICtTaiGZREPjJNnwVYxYvJAiZfikSW","expirationTime":null,"keys":{"p256dh":"BDWnX5H30jorPlPKCLg5M-bCQRGJ6wNGmG0MVmmFT4PnJG-G_m2_zxsscLCxq9EXx7pP652D5VT-vFbHE9XMLnc=","auth":"qiuJCfP7mpaDfCjgbEsHRQ=="}}
 
 new Web_Notification;
